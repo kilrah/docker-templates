9886f6beb30ef8f6bf253fb3ecff74c6c162158e
Kilrah <kilrah@kilrah.xyz>
2024-06-21 12:43:31 +0200

    Add PDlib for Facial Recognition app

--- nextcloud-ffmpeg/build.sh
+++ nextcloud-ffmpeg/build.sh
@@ -3,42 +3,39 @@
 function build_version {
 	local MAJOR_VERSION="${VERSION%%.*}"
 
-	sed -i '1s/.*/FROM nextcloud:'"${VERSION}"'/' Dockerfile
-	docker build -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
-	docker build -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
 	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 
 	VERSION="${VERSION}"-fpm
 	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
 
-	sed -i '1s/.*/FROM nextcloud:'"${VERSION}"'/' Dockerfile
-	docker build -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
-	docker build -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
 	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 }
 
 function build_default {
-	sed -i '1s/.*/FROM nextcloud:'"${VERSION}"'/' Dockerfile
-	docker build -t kilrah/nextcloud-ffmpeg .
+	docker build -build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg .
 	docker push kilrah/nextcloud-ffmpeg
 
 	VERSION="${VERSION}"-fpm
 
-	sed -i '1s/.*/FROM nextcloud:'"${VERSION}"'/' Dockerfile
-	docker build -t kilrah/nextcloud-ffmpeg:fpm .
+	docker build -build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:fpm .
 	docker push kilrah/nextcloud-ffmpeg:fpm
 }
 
+BUILDERVERSION=29
 #VERSION=26.0.13
 #build_version
-#VERSION=27.1.9
-#build_version
-#VERSION=28.0.5
-#build_version
-VERSION=29.0.0
+VERSION=27.1.10
+build_version
+VERSION=28.0.6
+build_version
+VERSION=29.0.2
 build_version
 
-#VERSION=28.0.5
-#build_default
+VERSION=29.0.2
+build_default
