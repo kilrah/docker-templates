009a4bbc4b78a2199db8f233c725ff0602fc8904
Kilrah <kilrah@users.noreply.github.com>
2025-04-07 10:40:50 +0200

    nextcloud-ffmpeg: Add support for v31

--- nextcloud-ffmpeg/build.sh
+++ nextcloud-ffmpeg/build.sh
@@ -17,25 +17,48 @@ function build_version {
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 }
 
+function build_version_31 {
+	local MAJOR_VERSION="${VERSION%%.*}"
+
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-31
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .  -f Dockerfile-31
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
+
+	VERSION="${VERSION}"-fpm
+	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
+
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${VERSION}" .  -f Dockerfile-31
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .  -f Dockerfile-31
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
+}
+
 function build_default {
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg .
+	docker tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg
 	docker push kilrah/nextcloud-ffmpeg
 
 	VERSION="${VERSION}"-fpm
 
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} -t kilrah/nextcloud-ffmpeg:fpm .
+	docker tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg:fpm
 	docker push kilrah/nextcloud-ffmpeg:fpm
 }
 
-BUILDERVERSION=29
+BUILDERVERSION=30
 #VERSION=26.0.13
 #build_version
-VERSION=27.1.10
+#VERSION=27.1.11
+#build_version
+#VERSION=28.0.14
+#build_version
+VERSION=29.0.14
 build_version
-VERSION=28.0.6
-build_version
-VERSION=29.0.2
+VERSION=30.0.8
 build_version
 
-VERSION=29.0.2
+BUILDERVERSION=31
+VERSION=31.0.2
+build_version_31
+
+VERSION=31.0.2
 build_default
