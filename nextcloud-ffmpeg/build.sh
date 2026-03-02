ca19ae6dade481614f2c8312387e63b0b7587015
Kilrah <kilrah@users.noreply.github.com>
2026-03-02 21:25:22 +0100

    nextcloud-ffmpeg: update build for 33

--- nextcloud-ffmpeg/build.sh
+++ nextcloud-ffmpeg/build.sh
@@ -34,7 +34,24 @@ function build_version_31 {
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 }
 
-function build_default {
+function build_version_33 {
+	local MAJOR_VERSION="${VERSION%%.*}"
+
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-33
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" . -f Dockerfile-33
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
+
+	VERSION="${VERSION}"-fpm
+	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
+
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-33
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" . -f Dockerfile-33
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
+}
+
+function set_default {
 	docker tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg
 	docker push kilrah/nextcloud-ffmpeg
 
@@ -44,25 +61,18 @@ function build_default {
 	docker push kilrah/nextcloud-ffmpeg:fpm
 }
 
-#BUILDERVERSION=30
-#VERSION=26.0.13
-#build_version
-#VERSION=27.1.11
-#build_version
-#VERSION=28.0.14
-#build_version
-#VERSION=29.0.16
-#build_version
-#BUILDERVERSION=31
-#VERSION=30.0.17
-#build_version_31
-
 BUILDERVERSION=31.0.14
+
 VERSION=31.0.14
 build_version_31
 
 VERSION=32.0.6
 build_version_31
 
+BUILDERVERSION=33.0.0
+
+VERSION=33.0.0
+build_version_33
+
 VERSION=32.0.6
-build_default
+set_default
