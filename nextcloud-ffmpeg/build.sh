b874204d3c2f88903eaa01294a06460aa5535139
Kilrah <kilrah@kilrah.xyz>
2026-03-02 22:16:06 +0100

    nextcloud-ffmpeg: simplify build script

--- nextcloud-ffmpeg/build.sh
+++ nextcloud-ffmpeg/build.sh
@@ -1,78 +1,43 @@
 #!/bin/bash
 
-function build_version {
+function build_images {
 	local MAJOR_VERSION="${VERSION%%.*}"
+	local VERSION_FPM="${VERSION}"-fpm
+	local MAJOR_VERSION_FPM="${MAJOR_VERSION}"-fpm
 
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-
-	VERSION="${VERSION}"-fpm
-	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
-
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" .
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}" .
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-}
-
-function build_version_31 {
-	local MAJOR_VERSION="${VERSION%%.*}"
-
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-31
+	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-"${MAJOR_VERSION}"
 	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 
-	VERSION="${VERSION}"-fpm
-	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
-
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" .  -f Dockerfile-31
-	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-}
-
-function build_version_33 {
-	local MAJOR_VERSION="${VERSION%%.*}"
-
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-33
-	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-
-	VERSION="${VERSION}"-fpm
-	MAJOR_VERSION="${MAJOR_VERSION}"-fpm
-
-	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-33
-	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
+	docker build --build-arg VERSION=${VERSION_FPM} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION_FPM}" . -f Dockerfile-"${MAJOR_VERSION}"
+	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION_FPM}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION_FPM}"
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION_FPM}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION_FPM}"
 }
 
 function set_default {
-	docker tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg
+	docker image tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg
 	docker push kilrah/nextcloud-ffmpeg
 
 	VERSION="${VERSION}"-fpm
 
-	docker tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg:fpm
+	docker image tag kilrah/nextcloud-ffmpeg:${VERSION} kilrah/nextcloud-ffmpeg:fpm
 	docker push kilrah/nextcloud-ffmpeg:fpm
 }
 
 BUILDERVERSION=31.0.14
 
 VERSION=31.0.14
-build_version_31
+build_images
 
 VERSION=32.0.6
-build_version_31
+build_images
 
 BUILDERVERSION=33.0.0
 
 VERSION=33.0.0
-build_version_33
+build_images
 
 VERSION=32.0.6
 set_default
