207ef0639a302cb75b9368dc18586e5269ea9c04
Kilrah <kilrah@kilrah.xyz>
2026-06-29 15:13:06 +0200

    nextcloud-ffmpeg: add 34

--- nextcloud-ffmpeg/build.sh
+++ nextcloud-ffmpeg/build.sh
@@ -7,11 +7,18 @@ function build_images {
 
 	docker build --build-arg VERSION=${VERSION} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION}" . -f Dockerfile-"${MAJOR_VERSION}"
 	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
-	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
-
+	
 	docker build --build-arg VERSION=${VERSION_FPM} --build-arg BUILDERVERSION=${BUILDERVERSION} --pull -t kilrah/nextcloud-ffmpeg:"${VERSION_FPM}" . -f Dockerfile-"${MAJOR_VERSION}"
 	docker image tag kilrah/nextcloud-ffmpeg:"${VERSION_FPM}" kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION_FPM}"
+}
+
+function push_images {
+	local MAJOR_VERSION="${VERSION%%.*}"
+	local VERSION_FPM="${VERSION}"-fpm
+	local MAJOR_VERSION_FPM="${MAJOR_VERSION}"-fpm
+
+	docker push kilrah/nextcloud-ffmpeg:"${VERSION}"
+	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION}"
 	docker push kilrah/nextcloud-ffmpeg:"${VERSION_FPM}"
 	docker push kilrah/nextcloud-ffmpeg:"${MAJOR_VERSION_FPM}"
 }
@@ -26,18 +33,21 @@ function set_default {
 	docker push kilrah/nextcloud-ffmpeg:fpm
 }
 
-BUILDERVERSION=31.0.14
+BUILDERVERSION=32.0.11
 
-VERSION=31.0.14
+VERSION=32.0.11
 build_images
+push_images
 
-VERSION=32.0.6
+BUILDERVERSION=33.0.5
+
+VERSION=33.0.5
 build_images
+push_images
 
-BUILDERVERSION=33.0.0
-
-VERSION=33.0.0
+VERSION=34.0.0
 build_images
+push_images
 
-VERSION=32.0.6
+VERSION=33.0.5
 set_default
